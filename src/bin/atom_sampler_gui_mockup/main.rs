ff9e32cbbe20f95e5e73f6c399cbeb7b9206fbb2
MajorX234 <majorx234@googlemail.com>
2024-06-03 23:59:58 +0200

    Add gui creation

--- src/bin/atom_sampler_gui_mockup/main.rs
+++ src/bin/atom_sampler_gui_mockup/main.rs
@@ -4,4 +4,14 @@ use mockup::MockupGUI;
 
 fn main() {
     let mockup_gui = MockupGUI::default();
+    let options = eframe::NativeOptions {
+        viewport: ViewportBuilder::default().with_inner_size([600.0, 600.0]),
+        ..Default::default()
+    };
+
+    let _ = eframe::run_native(
+        "Atom Sampler MockupGUI",
+        options,
+        Box::new(|_cc| Box::new(mockup_gui)),
+    );
 }
