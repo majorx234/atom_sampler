014c58bd4a7a833b1b4f5f5db9cc3e650feae49b
MajorX234 <majorx234@googlemail.com>
2024-06-04 23:17:53 +0200

    Show pad_buttom in mockup Gui

--- src/bin/atom_sampler_gui_mockup/mockup.rs
+++ src/bin/atom_sampler_gui_mockup/mockup.rs
@@ -1,13 +1,25 @@
+use atom_sampler_lib::ui::elements::pad_button;
 use eframe::egui::{self, ViewportCommand};
 
-pub struct MockupGUI {}
+pub struct MockupGUI {
+    wave_loaded: bool,
+}
 
 impl Default for MockupGUI {
     fn default() -> Self {
-        Self {}
+        Self { wave_loaded: false }
     }
 }
 
 impl eframe::App for MockupGUI {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {}
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::TopBottomPanel::top("control").show(ctx, |ui| {
+            ui.vertical(|ui| {
+                ui.heading("MockupGui");
+            })
+        });
+        egui::CentralPanel::default().show(ctx, |ui| {
+            pad_button(ui, &self.wave_loaded);
+        });
+    }
 }
