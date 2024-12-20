16ed228efa88469c76ead2779847766bdfc3352c
MajorX234 <majorx234@googlemail.com>
2024-12-20 23:45:41 +0100

    Add method to add direct data to sample

--- src/dsp/sample.rs
+++ src/dsp/sample.rs
@@ -6,7 +6,6 @@ use std::path::{Path, PathBuf};
 struct Sample {
     data_left: Vec<f32>,
     data_right: Vec<f32>,
-    path: Option<Box<Path>>,
     gain: f32,
     speed: f32,
     pan: f32,
@@ -17,14 +16,20 @@ impl Sample {
         Sample {
             data_left: Vec::new(),
             data_right: Vec::new(),
-            path: None,
             gain: 1.0f32,
             speed: 1.0f32,
             pan: 0.5f32,
         }
     }
-    fn load_from_wav() -> Result<Self> {
-        let mut reader = hound::WavReader::open("testsamples/pop.wav")?;
+
+    fn load_from_data(&mut self, data_left: Vec<f32>, data_right: Vec<f32>) -> Result<()> {
+        self.data_left = data_left;
+        self.data_right = data_right;
+        Ok(())
+    }
+
+    fn load_from_wav(&mut self, path: PathBuf) -> Result<()> {
+        let mut reader = hound::WavReader::open(path)?;
         let format = reader.spec().sample_format;
         let mut nsamples = reader.len() as usize;
         match format {
@@ -33,14 +38,9 @@ impl Sample {
                     .samples::<f32>()
                     .map(|x| x.unwrap())
                     .collect::<Vec<f32>>();
-                Ok(Sample {
-                    data_left: Vec::new(),
-                    data_right: Vec::new(),
-                    path: None,
-                    gain: 1.0f32,
-                    speed: 1.0f32,
-                    pan: 0.5f32,
-                })
+                self.data_left = data.clone();
+                self.data_right = data;
+                Ok(())
             }
             _ => Err(Error::IoWrongDatatyp),
         }
