f8edc0fd1dc447decef5c4f6a5937e51ed292cdf
MajorX234 <majorx234@googlemail.com>
2025-02-10 23:57:26 +0100

    Add wave file reading

--- src/bin/wave_load_gui/wave_load_gui.rs
+++ src/bin/wave_load_gui/wave_load_gui.rs
@@ -1,22 +1,27 @@
 use atom_sampler_lib::ui::elements::{pad_button, pad_button_ui, DebugConsole};
 use eframe::egui::{self, menu, Button, Context, PointerButton, ViewportCommand, Widget};
+use hound;
 use std::path::PathBuf;
 
 pub struct WaveLoadGUI {
+    pub file_name_str: String,
     pub wave_loaded: bool,
     pub console: DebugConsole,
     picked_file: Option<PathBuf>,
+    wave_data: Option<Vec<f32>>,
 }
 
 impl Default for WaveLoadGUI {
     fn default() -> Self {
         Self {
+            file_name_str: String::new(),
             wave_loaded: false,
             console: DebugConsole {
                 n_items: 0,
                 msgs: Vec::new(),
             },
             picked_file: None,
+            wave_data: Some(Vec::new()),
         }
     }
 }
@@ -28,6 +33,21 @@ impl eframe::App for WaveLoadGUI {
             })
         });
         egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("Filepath:");
+            ui.text_edit_singleline(&mut self.file_name_str);
+            if ui.button("load file").clicked() {
+                self.picked_file = Some(PathBuf::from(&self.file_name_str));
+                if let Some(ref picked_file) = self.picked_file {
+                    self.console
+                        .add_entry(picked_file.to_str().get_or_insert("none").to_string());
+                }
+            }
+            if let Some(ref picked_file) = self.picked_file {
+                if picked_file.is_file() {
+                    let mut reader = hound::WavReader::open(picked_file).unwrap();
+                    self.wave_data = Some(reader.samples::<f32>().flatten().collect());
+                }
+            }
             self.console.debug_console_ui(ui);
         });
     }
