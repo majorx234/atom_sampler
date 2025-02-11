8b162f3c14f6b4f77ea8f42d4fef229f8761380b
MajorX234 <majorx234@googlemail.com>
2025-02-11 02:31:02 +0100

    Add function to load wave files

--- src/bin/wave_load_gui/wave_load_gui.rs
+++ src/bin/wave_load_gui/wave_load_gui.rs
@@ -3,6 +3,21 @@ use eframe::egui::{self, menu, Button, Context, PointerButton, ViewportCommand, 
 use hound;
 use std::path::PathBuf;
 
+fn read_wav_file(file_path: &PathBuf) -> Result<Vec<f32>, hound::Error> {
+    let mut reader = hound::WavReader::open(file_path).unwrap();
+    let spec = reader.spec();
+
+    let wave_data: Vec<f32> = match spec.sample_format {
+        hound::SampleFormat::Float => reader.samples::<f32>().flatten().collect(),
+        hound::SampleFormat::Int => reader
+            .samples::<i16>()
+            .flatten()
+            .map(|x| x as f32 / i16::MAX as f32)
+            .collect(),
+    };
+
+    Ok(wave_data)
+}
 pub struct WaveLoadGUI {
     pub file_name_str: String,
     pub wave_loaded: bool,
@@ -36,16 +51,24 @@ impl eframe::App for WaveLoadGUI {
             ui.label("Filepath:");
             ui.text_edit_singleline(&mut self.file_name_str);
             if ui.button("load file").clicked() {
+                self.wave_loaded = false;
                 self.picked_file = Some(PathBuf::from(&self.file_name_str));
                 if let Some(ref picked_file) = self.picked_file {
                     self.console
                         .add_entry(picked_file.to_str().get_or_insert("none").to_string());
                 }
             }
-            if let Some(ref picked_file) = self.picked_file {
-                if picked_file.is_file() {
-                    let mut reader = hound::WavReader::open(picked_file).unwrap();
-                    self.wave_data = Some(reader.samples::<f32>().flatten().collect());
+            if ui.button("load wave").clicked() {
+                if let Some(ref picked_file) = self.picked_file {
+                    if picked_file.is_file() {
+                        if let Ok(samples_vec) = read_wav_file(picked_file) {
+                            let num_samples = samples_vec.len();
+                            self.wave_data = Some(samples_vec);
+                            self.wave_loaded = true;
+                            self.console
+                                .add_entry(format!("wave loaded: {} samples", num_samples));
+                        }
+                    }
                 }
             }
             self.console.debug_console_ui(ui);
