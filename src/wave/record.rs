29baceee7c416ee50c479079ed8a6871b23763cd
MajorX234 <majorx234@googlemail.com>
2025-03-02 00:00:10 +0100

    Add small waiting time in polling recording thread

--- src/wave/record.rs
+++ src/wave/record.rs
@@ -3,6 +3,7 @@ use ringbuf::{
     traits::{Consumer, Observer, Producer},
     HeapCons, HeapProd,
 };
+use std::{thread, time};
 
 pub fn start_recording(
     mut ringbuffer_left_in: HeapCons<f32>,
@@ -23,47 +24,53 @@ pub fn start_recording(
         while run {
             let length_left = 1024.min(ringbuffer_left_in.occupied_len());
             let length_right = 1024.min(ringbuffer_right_in.occupied_len());
-
-            if (vecpointer_left + length_left < wave_size)
-                && (vecpointer_right + length_right < wave_size)
-            {
-                wave_left.splice(
-                    vecpointer_left..vecpointer_left + length_left,
-                    ringbuffer_left_in.pop_iter(),
-                );
-                let max_l = wave_left[vecpointer_left..vecpointer_left + length_left]
-                    .iter()
-                    .max_by(|a, b| a.total_cmp(b))
-                    .unwrap_or(&0.0);
-                let min_l = wave_left[vecpointer_left..vecpointer_left + length_left]
-                    .iter()
-                    .min_by(|a, b| a.total_cmp(b))
-                    .unwrap_or(&0.0);
-                vecpointer_left += length_left;
-                wave_right.splice(
-                    vecpointer_right..vecpointer_right + length_right,
-                    ringbuffer_right_in.pop_iter(),
-                );
-                let max_r = wave_right[vecpointer_right..vecpointer_right + length_right]
-                    .iter()
-                    .max_by(|a, b| a.total_cmp(b))
-                    .unwrap_or(&0.0);
-                let min_r = wave_right[vecpointer_right..vecpointer_right + length_right]
-                    .iter()
-                    .min_by(|a, b| a.total_cmp(b))
-                    .unwrap_or(&0.0);
-                vecpointer_right += length_right;
-                if let Some(ref mut ringbuffer_left_visual_in) = ringbuffer_left_visual_in_opt {
-                    let _ = ringbuffer_left_visual_in.try_push((*max_l, *min_l));
-                }
-                if let Some(ref mut ringbuffer_right_visual_in) = ringbuffer_right_visual_in_opt {
-                    let _ = ringbuffer_right_visual_in.try_push((*max_r, *min_r));
-                }
+            if length_left == 0 && length_right == 0 {
+                thread::sleep(time::Duration::from_millis(10));
             } else {
-                ringbuffer_left_in.clear();
-                ringbuffer_right_in.clear();
-                run = false;
-                println!("start_recording: wave is full");
+                if (vecpointer_left + length_left < wave_size)
+                    && (vecpointer_right + length_right < wave_size)
+                {
+                    wave_left.splice(
+                        vecpointer_left..vecpointer_left + length_left,
+                        ringbuffer_left_in.pop_iter(),
+                    );
+                    let max_l = wave_left[vecpointer_left..vecpointer_left + length_left]
+                        .iter()
+                        .max_by(|a, b| a.total_cmp(b))
+                        .unwrap_or(&0.0);
+                    let min_l = wave_left[vecpointer_left..vecpointer_left + length_left]
+                        .iter()
+                        .min_by(|a, b| a.total_cmp(b))
+                        .unwrap_or(&0.0);
+                    vecpointer_left += length_left;
+
+                    wave_right.splice(
+                        vecpointer_right..vecpointer_right + length_right,
+                        ringbuffer_right_in.pop_iter(),
+                    );
+                    let max_r = wave_right[vecpointer_right..vecpointer_right + length_right]
+                        .iter()
+                        .max_by(|a, b| a.total_cmp(b))
+                        .unwrap_or(&0.0);
+                    let min_r = wave_right[vecpointer_right..vecpointer_right + length_right]
+                        .iter()
+                        .min_by(|a, b| a.total_cmp(b))
+                        .unwrap_or(&0.0);
+                    vecpointer_right += length_right;
+
+                    if let Some(ref mut ringbuffer_left_visual_in) = ringbuffer_left_visual_in_opt {
+                        let _ = ringbuffer_left_visual_in.try_push((*max_l, *min_l));
+                    }
+                    if let Some(ref mut ringbuffer_right_visual_in) = ringbuffer_right_visual_in_opt
+                    {
+                        let _ = ringbuffer_right_visual_in.try_push((*max_r, *min_r));
+                    }
+                } else {
+                    ringbuffer_left_in.clear();
+                    ringbuffer_right_in.clear();
+                    run = false;
+                    println!("start_recording: wave is full");
+                }
             }
 
             if let Ok(is_stop) = rx_stop_rec.try_recv() {
